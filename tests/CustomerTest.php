638cb594a490be7f9ee593569126db58303c0e15
everzet <ever.zet@gmail.com>
2014-07-09 11:01:49 +0100

    Write a first functional test case

---
+++ tests/CustomerTest.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Magenager
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the MIT License, that is bundled with this
+ * package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ *
+ * http://opensource.org/licenses/MIT
+ *
+ * If you did not receive a copy of the license and are unable to obtain it
+ * through the world-wide-web, please send an email
+ * to <magetest@sessiondigital.com> so we can send you a copy immediately.
+ *
+ * @copyright  Copyright (c) 2014 MageTest team and contributors.
+ */
+namespace MageTest\Magenager;
+
+use Behat\Mink\Driver\GoutteDriver;
+use Behat\Mink\Mink;
+use Behat\Mink\Session;
+use Behat\Mink\WebAssert;
+
+class CustomerTest extends \PHPUnit_Framework_TestCase
+{
+    private $mink;
+    private $fixtures;
+    private $customer;
+
+    protected function setUp()
+    {
+        $this->mink = new Mink(array(
+            'goutte' => new Session(new GoutteDriver())
+        ));
+        $this->mink->setDefaultSessionName('goutte');
+
+        $this->fixtures = new Customer();
+    }
+
+    protected function tearDown()
+    {
+        if ($this->customer) {
+            $this->fixtures->delete($this->customer);
+        }
+    }
+
+    public function createsCustomerWithEmailAndFirstName()
+    {
+        $email = 'ever.zet@gmail.com';
+        $pass = 'qwerty';
+
+        $attributes = array(
+            "website_id"   => \Mage::app()->getWebsite()->getId(),
+            "store"        => \Mage::app()->getStore(),
+            "email"        => $email,
+            "firstname"    => "test",
+            "lastname"     => "test",
+            "password"     => $pass,
+            "confirmation" => $pass,
+            "status"       => 1
+        );
+
+        $this->fixtures->create($attributes);
+
+        $session = $this->getSession();
+        $session->visit(getenv('BASE_URL') . '/customer/account/login');
+        $session->getPage()->fillField('email', $email);
+        $session->getPage()->fillField('pass', $pass);
+        $session->getPage()->pressButton('send2');
+
+        $this->assertSession()->addressEquals('/customer/account');
+    }
+
+    /**
+     * @param null|string $name
+     *
+     * @return Session
+     */
+    protected function getSession($name = null)
+    {
+        return $this->mink->getSession($name);
+    }
+
+    /**
+     * @param null|string $name
+     *
+     * @return WebAssert
+     */
+    protected function assertSession($name = null)
+    {
+        return $this->mink->assertSession($name);
+    }
+}
